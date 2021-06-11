d8dba1e5e535074a93eae65ee8d2d441dac1cef5
Mike Mwanje <mwanjemike767@gmail.com>
2021-06-11 20:12:14 +0300

    Chapter 3: User Report

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -127,11 +127,14 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: User Report
       // Return the 20 users who have commented the most on MFlix.
-      const pipeline = []
+      const groupStage = { $group: { _id: "$email", count: { $sum: 1 } } }
+      const sortStage = { $sort: { count: -1 } }
+      const limitStage = { $limit: 20 }
+      const pipeline = [groupStage, sortStage, limitStage]
 
       // TODO Ticket: User Report
       // Use a more durable Read Concern here to make sure this data is not stale.
-      const readConcern = comments.readConcern
+      const readConcern = { level: "majority" }
 
       const aggregateResult = await comments.aggregate(pipeline, {
         readConcern,
